931db5727db155e045dc30696f9cccf26c7118c4
shadid12 <shadidhaque2012@gmail.com>
2022-03-31 01:31:21 -0400

    added marketplace init

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      hello worlds
+    </div>
+  )
+}
+
+export default Home
