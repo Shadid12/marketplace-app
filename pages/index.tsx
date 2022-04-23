2a22ff63eda63f6188eef15eb896a2c0bd522683
shadid12 <shadidhaque2012@gmail.com>
2022-04-23 13:16:07 -0400

    query simple data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_SHOP = gql`
+query {
+  findShopByID(id: "327780139217715792") {
+    _id
+    description
+    name
+    ownerID
+    products {
+      _id
+    }
+  }
+}
+`
 
 const Home: NextPage = () => {
+  const { data } = useQuery(GET_SHOP)
+  console.log(' ==> ', data)
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       hello worlds
