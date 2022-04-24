8aeaea11cead1495bc5de6ceb5413e9ba5635f19
shadid12 <shadidhaque2012@gmail.com>
2022-04-23 22:08:54 -0400

    video 7 done

---
+++ components/ProductList.tsx
@@ -0,0 +1,36 @@
+const ProductList = ({ products }: { products: any[] } ) => { 
+  return (
+    <div className={listStyle}>
+      <div className={listGridStyle}>
+      {products.map(product => 
+        <ProductItem key={product._id} product={product} />  
+      )}
+      </div>
+    </div>
+  )
+}
+
+const  ProductItem = ({ product } : { product: any }) => (
+  <a href="#" className="group">
+    <div className={itemContainer}>
+      <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" className={imageStyle} />
+    </div>
+    <h3 className="mt-4 text-sm text-gray-700">
+      {product.name}
+    </h3>
+    <p className="mt-1 text-lg font-medium text-gray-900">
+      $ {product.price}
+    </p>
+  </a>
+)
+
+
+const listStyle = `mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8`;
+
+const listGridStyle = `grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8`;
+
+const itemContainer = `aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200`;
+
+const imageStyle = `h-full w-full object-cover object-center group-hover:opacity-75`;
+
+export default ProductList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,28 @@
 import type { NextPage } from 'next'
 import { gql, useQuery } from '@apollo/client'
+import ProductList from '../components/ProductList'
 
-const GET_SHOP = gql`
+const GET_PRODUCTS = gql`
 query {
-  findShopByID(id: "327780139217715792") {
-    _id
-    description
-    name
-    ownerID
-    products {
+  getAllProducts {
+    data {
       _id
+      name
+      description
+      price
+      shop {
+        _id
+      }
     }
   }
 }
 `
 
 const Home: NextPage = () => {
-  const { data } = useQuery(GET_SHOP)
+  const { data } = useQuery(GET_PRODUCTS)
   console.log(' ==> ', data)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      hello worlds
-    </div>
+    <ProductList products={data.getAllProducts.data} />
   )
 }
 
