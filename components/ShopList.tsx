9e44bcd84a1f8b44c6606430c800a71ad38d296b
shadid12 <shadidhaque2012@gmail.com>
2022-06-06 15:22:05 -0400

    done video 14

---
+++ components/ShopList.tsx
@@ -0,0 +1,26 @@
+export default function ShopsList({ shops }: { shops: any }) {
+
+  return (
+    <div className="p-16">
+      <h1>Your Shops List</h1>
+      {
+        shops.map((shop: any) => (
+          <div key={shop._id} className="rounded-lg bg-white shadow mb-1">
+            <ul className="divide-y divide-gray-100">
+              <li className="flex justify-between p-3">
+                <h2>{shop.name}</h2>
+                <div>
+                  <button className={btnClass}>Add Product</button>
+                  <button className={deleteBtn}>Delete Shop</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+const btnClass = `rounded-full border border-transparent bg-indigo-600 px-3.5 py-2 text-white shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`;
+const deleteBtn = `ml-2 rounded-full border border-transparent bg-pink-600 px-3.5 py-2 text-white shadow-md hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2`
--- pages/manage-shops.tsx
+++ pages/manage-shops.tsx
@@ -1,8 +1,33 @@
 import NewShopForm from "../components/NewShopForm";
 import { getAccessToken } from '@auth0/nextjs-auth0';
+import { gql, useQuery } from '@apollo/client';
+import { useUser } from '@auth0/nextjs-auth0';
+import ShopList from "../components/ShopList";
+
+const GET_SHOPS_BY_OWNER = gql`
+  query getShopsByOwnerID($ownerID: String!) {
+    getShopsByOwnerID(ownerID: $ownerID) {
+      data {
+        _id
+        name
+      }
+    }
+  }
+`;
 
 export default function ManageShops(props: any) {
-  return <NewShopForm accessToken={props.accessToken}/>
+  const { user } = useUser();
+  const { data } = useQuery(GET_SHOPS_BY_OWNER, {
+    variables: { ownerID: user?.sub }
+  })
+  return (<>
+    <NewShopForm accessToken={props.accessToken}/>
+    {
+      data ? (
+        <ShopList shops={data.getShopsByOwnerID.data}/>
+      ) : <div>Loading...</div>
+    }
+  </>)
 }
 
 export async function getServerSideProps(ctx: any) {
